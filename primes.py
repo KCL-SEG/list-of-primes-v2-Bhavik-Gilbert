0740963e638a221d475056257096998078aeb543
Bhavik Glibert <bhaviklob@gmail.com>
2022-09-26 19:43:16 +0100

    Prime Function Implemented
    Working Primes function, returning list of prime numbers length specified. ValueError given if argument given is less than or equal to 0.

--- primes.py
+++ primes.py
@@ -3,4 +3,22 @@
 
 def primes(number_of_primes):
     list = []
+    number = 2
+
+    if number_of_primes <= 0:
+        raise ValueError
+
+    while(len(list) < number_of_primes):
+        is_prime = True
+
+        for prime in list:
+            if(number % prime == 0):
+                is_prime = False
+                break
+
+        if(is_prime):
+            list.append(number)
+        
+        number += 1
+
     return list
